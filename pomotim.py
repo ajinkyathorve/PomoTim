1796ef12ccc1eda7b536a299a803aa96e3e9a7c1
ajinkyathorve <ajinkyathorve@yahoo.com>
2017-10-24 07:40:08 -0400

    Added functions for info and alert

--- pomotim.py
+++ pomotim.py
@@ -9,13 +9,61 @@ Created on Tue Sep 12 21:40:31 2017
 
 import Tkinter as tk
 from PIL import Image, ImageTk
+import webbrowser
+import pyaudio
+import wave
+from random import randint
 
 
 def countdown(time, time_list):
+       progress = tk.Label(text="In Progress", font=("Arial", 12)).grid(row=10-len(time_list),column=1,sticky=('W'),padx=20,pady=5)
+       mins, secs = divmod(time, 60)
+       timeformat = '{:02d}:{:02d}'.format(mins, secs)
+       time_label['text'] = timeformat
+       if time > 0:
+              root.after(1000, countdown, time-1, time_list)
+       else:
+              play_alert_sound()
+              progress = tk.Label(text="*Completed*", font=("Arial", 12)).grid(row=10-len(time_list),column=1,sticky=('W'),padx=20,pady=5)
+              if (time_list):
+                     time = time_list.pop()
+                     countdown(time, time_list)
+              else:
+                     finish_text = "Congratulations! You just finished the set of tasks.\nPlease close the application and start again if you want to begin another set."
+                     finish_label = tk.Label(root, text=finish_text, font=("Arial", 16))
+                     finish_label.grid(row=11,column=0,columnspan=1,sticky=('N','S','E','W'),padx=25,pady=25)
+                     image = Image.open("./"+str(randint(1,3))+".jpg")
+                     finish_image = ImageTk.PhotoImage(image)
+#                     finish_image = tk.PhotoImage(file="./i_helps.gif")
+                     finish_image_label = tk.Label(root, image=finish_image)
+                     finish_image_label.image = finish_image
+                     finish_image_label.grid(row=12,column=0,columnspan=1,sticky=('N','S','E','W'),padx=25,pady=25)
        return
 
 
 def more_info():
+       webbrowser.open_new_tab("https://en.wikipedia.org/wiki/Pomodoro_Technique")
+       return
+
+
+def play_alert_sound():
+       chunk = 1024
+       wf = wave.open('./audio/alert.wav', 'rb')
+       p = pyaudio.PyAudio()
+       
+       stream = p.open(
+                       format = p.get_format_from_width(wf.getsampwidth()),
+                       channels = wf.getnchannels(),
+                       rate = wf.getframerate(),
+                       output = True)
+       data = wf.readframes(chunk)
+       
+       while data != '':
+           stream.write(data)
+           data = wf.readframes(chunk)
+       
+       stream.close()
+       p.terminate()
        return
 
 
